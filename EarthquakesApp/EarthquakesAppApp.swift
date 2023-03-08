4738f8e00426aa64758770a1ec0e6da7fce7509e
Alex Rybachev <rybachev.as@gmail.com>
2023-03-08 11:03:34 +0400

    Initial Commit

---
+++ EarthquakesApp/EarthquakesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthquakesAppApp.swift
+//  EarthquakesApp
+//
+//  Created by Aleksandr Rybachev on 08.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct EarthquakesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
